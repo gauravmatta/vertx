2ba91f28d28a0c4d43aab1cf46560ce0148699a9
gauravmatta <gaurav.matta.tech@gmail.com>
2024-11-11 18:31:10 +0530

    added Worker Verticle Example

---
+++ vertx-starter/src/main/java/com/vertximplant/starter/vertx_starter/worker/WorkerVerticle.java
@@ -0,0 +1,19 @@
+package com.vertximplant.starter.vertx_starter.worker;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Promise;
+import io.vertx.core.Vertx;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class WorkerVerticle extends AbstractVerticle {
+  private static final Logger LOG = LoggerFactory.getLogger(WorkerVerticle.class);
+
+  @Override
+  public void start(Promise<Void> startPromise) throws Exception {
+    LOG.debug("Deployed as a Worker Vertical");
+    startPromise.complete();
+    Thread.sleep(5000);
+    LOG.debug("Blocking Operation Done");
+  }
+}
