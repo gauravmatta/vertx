29e5fa6b037ff415289ddf6322d25c47227a81d9
gauravmatta <gaurav.matta.tech@gmail.com>
2024-11-11 11:35:51 +0530

    added logging

--- vertx-starter/src/main/java/com/vertximplant/starter/vertx_starter/verticles/VerticleN.java
+++ vertx-starter/src/main/java/com/vertximplant/starter/vertx_starter/verticles/VerticleN.java
@@ -2,13 +2,17 @@ package com.vertximplant.starter.vertx_starter.verticles;
 
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Promise;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class VerticleN extends AbstractVerticle {
 
+  private static final Logger LOG = LoggerFactory.getLogger(VerticleN.class);
+
   @Override
   public void start(final Promise<Void> startPromise) throws Exception {
-    System.out.println("Start " + getClass().getName() +" on thread "+ Thread.currentThread().getName());
-    System.out.println("With config options "+ config().toString());
+    LOG.debug("Start {}", getClass().getName() +" on thread "+ Thread.currentThread().getName());
+    LOG.debug("With config options {}", config().toString());
     startPromise.complete();
   }
 
