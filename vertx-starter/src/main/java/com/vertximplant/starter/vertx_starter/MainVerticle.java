d116a0a9f2ae701216c087813ea8219d2b966a0f
Gaurav Matta <gaurav.matta.tech@gmail.com>
2024-12-31 10:21:25 +0530

    Added Dockerfile and MainVerticle and Main Verticle Launcher

--- vertx-starter/src/main/java/com/vertximplant/starter/vertx_starter/MainVerticle.java
+++ vertx-starter/src/main/java/com/vertximplant/starter/vertx_starter/MainVerticle.java
@@ -3,8 +3,14 @@ package com.vertximplant.starter.vertx_starter;
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Promise;
 import io.vertx.core.Vertx;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Random;
 
 public class MainVerticle extends AbstractVerticle {
+
+  private static final Logger LOG = LoggerFactory.getLogger(MainVerticle.class);
 
   public static void main(String[] args) {
   Vertx vertx =  Vertx.vertx();
@@ -24,6 +30,10 @@ public class MainVerticle extends AbstractVerticle {
       } else {
         startPromise.fail(http.cause());
       }
+    });
+    vertx.setPeriodic(500,id ->{
+      LOG.debug("Redeploy...");
+      LOG.debug(String.valueOf(new Random().nextDouble()));
     });
   }
 }
