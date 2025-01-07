eb7eebf5f6441592527c0786904a3f4c2eeb8d6b
Gaurav Matta <gaurav.matta.tech@gmail.com>
2025-01-07 08:57:35 +0530

    Added Logger logback-classic and Logging Framework

--- vertx-big-board/src/main/java/com/vertximplant/starter/vertx_big_board/MainVerticle.java
+++ vertx-big-board/src/main/java/com/vertximplant/starter/vertx_big_board/MainVerticle.java
@@ -2,8 +2,27 @@ package com.vertximplant.starter.vertx_big_board;
 
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Promise;
+import io.vertx.core.Vertx;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class MainVerticle extends AbstractVerticle {
+
+  private static final Logger LOG = LoggerFactory.getLogger(MainVerticle.class);
+
+  public static void main(String[] args) {
+    Vertx vertx = Vertx.vertx();
+    vertx.exceptionHandler(error ->{
+      LOG.error("Unhandled:",error);
+    });
+    vertx.deployVerticle(new MainVerticle(),ar ->{
+      if(ar.failed()) {
+        LOG.error("Failed to deploy:",ar.cause());
+        return;
+      }
+      LOG.info("Deployed:{}!",MainVerticle.class.getName());
+    });
+  }
 
   @Override
   public void start(Promise<Void> startPromise) throws Exception {
