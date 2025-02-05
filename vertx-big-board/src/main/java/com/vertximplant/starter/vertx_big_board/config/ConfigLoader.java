cc54646a7a8e1713ebb171b26955d798f52ac5be
Gaurav Matta <gaurav.matta.tech@gmail.com>
2025-02-05 14:01:29 +0530

    Added Envirnoment Variables

---
+++ vertx-big-board/src/main/java/com/vertximplant/starter/vertx_big_board/config/ConfigLoader.java
@@ -0,0 +1,33 @@
+package com.vertximplant.starter.vertx_big_board.config;
+
+import io.vertx.config.ConfigRetriever;
+import io.vertx.config.ConfigRetrieverOptions;
+import io.vertx.config.ConfigStoreOptions;
+import io.vertx.core.Future;
+import io.vertx.core.Vertx;
+import io.vertx.core.json.JsonArray;
+import io.vertx.core.json.JsonObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import java.util.List;
+
+public class ConfigLoader {
+  public static final String SERVER_PORT = "SERVER_PORT";
+  static final List<String> EXPOSED_ENVIRONMENT_VARIABLES = List.of(SERVER_PORT);
+  private static final Logger LOG = LoggerFactory.getLogger(ConfigLoader.class);
+
+  public static Future<BrokerConfig> loadBrokerConfig(Vertx vertx) {
+
+    final JsonArray exposedKeys = new JsonArray();
+    EXPOSED_ENVIRONMENT_VARIABLES.forEach(exposedKeys::add);
+    LOG.debug("Fetch Configuration from Exposed keys: {}", exposedKeys);
+
+    ConfigStoreOptions envStore = new ConfigStoreOptions().setType("env")
+        .setConfig(new JsonObject().put("keys", exposedKeys));
+
+    ConfigRetriever configRetriever =
+        ConfigRetriever.create(vertx, new ConfigRetrieverOptions().addStore(envStore));
+
+    return configRetriever.getConfig().map(BrokerConfig::from);
+  }
+}
