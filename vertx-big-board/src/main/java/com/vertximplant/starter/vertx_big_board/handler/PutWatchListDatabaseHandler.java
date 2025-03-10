1fab775816b3793f3c161cbeb0ebc557e6981b03
Gaurav Matta <gaurav.matta.tech@gmail.com>
2025-03-10 12:17:46 +0530

    Optimized Imports

--- vertx-big-board/src/main/java/com/vertximplant/starter/vertx_big_board/handler/PutWatchListDatabaseHandler.java
+++ vertx-big-board/src/main/java/com/vertximplant/starter/vertx_big_board/handler/PutWatchListDatabaseHandler.java
@@ -13,8 +13,8 @@ import io.vertx.sqlclient.Pool;
 import io.vertx.sqlclient.templates.SqlTemplate;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
 import java.util.HashMap;
-import java.util.Map;
 
 public class PutWatchListDatabaseHandler {
 
