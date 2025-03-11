362daf5d85a3c5d7114651ab7d877c60895ed858
Gaurav Matta <gaurav.matta.tech@gmail.com>
2025-03-11 08:31:07 +0530

    formatted code

--- vertx-big-board/src/main/java/com/vertximplant/starter/vertx_big_board/handler/PutWatchListDatabaseHandler.java
+++ vertx-big-board/src/main/java/com/vertximplant/starter/vertx_big_board/handler/PutWatchListDatabaseHandler.java
@@ -5,8 +5,6 @@ import com.google.inject.Inject;
 import com.vertximplant.starter.vertx_big_board.api.WatchListRestAPI;
 import com.vertximplant.starter.vertx_big_board.helper.DBResponseHelper;
 import com.vertximplant.starter.vertx_big_board.pojo.WatchList;
-import com.vertximplant.starter.vertx_big_board.pojo.exception.Failure;
-import io.netty.handler.codec.http.HttpResponseStatus;
 import io.vertx.core.json.JsonObject;
 import io.vertx.ext.web.RoutingContext;
 import io.vertx.sqlclient.Pool;
@@ -17,7 +15,6 @@ import org.slf4j.LoggerFactory;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 public class PutWatchListDatabaseHandler {
 
@@ -30,7 +27,7 @@ public class PutWatchListDatabaseHandler {
     String accountId = WatchListRestAPI.getAccountId(routingContext);
     JsonObject jsonObject = routingContext.body().asJsonObject();
     WatchList watchList = jsonObject.mapTo(WatchList.class);
-//    watchList.getAssets().forEach(asset -> {
+    // watchList.getAssets().forEach(asset -> {
     List<Map<String, Object>> parameterBatch = watchList.getAssets().stream().map(asset -> {
       final Map<String, Object> parameters = new HashMap<>();
       parameters.put("account_id", accountId);
@@ -39,17 +36,17 @@ public class PutWatchListDatabaseHandler {
     }).toList();
 
     SqlTemplate
-          .forUpdate(db,
-              "INSERT INTO broker.watchlist (account_id, asset) VALUES(#{account_id},#{asset})")
-          .executeBatch(parameterBatch).onFailure(DBResponseHelper.errorHandler(routingContext,
-              "Failed to insert watchlist for accountId: " + accountId))
-          .onSuccess(result -> {
-            dbResponseHelper.handleSuccessResponse("t_test", routingContext,result, stopwatch);
-//              dbResponseHelper.handleEmptyResponse("t_test", routingContext, "WatchListId",
-//                  new Failure(HttpResponseStatus.NO_CONTENT.code(), ""), "Insert in WatchList",
-//                  stopwatch);
-//
-          });
-//    });
+        .forUpdate(db,
+            "INSERT INTO broker.watchlist (account_id, asset) VALUES(#{account_id},#{asset})")
+        .executeBatch(parameterBatch).onFailure(DBResponseHelper.errorHandler(routingContext,
+            "Failed to insert watchlist for accountId: " + accountId))
+        .onSuccess(result -> {
+          dbResponseHelper.handleSuccessResponse("t_test", routingContext, result, stopwatch);
+          // dbResponseHelper.handleEmptyResponse("t_test", routingContext, "WatchListId",
+          // new Failure(HttpResponseStatus.NO_CONTENT.code(), ""), "Insert in WatchList",
+          // stopwatch);
+          //
+        });
+    // });
   }
 }
