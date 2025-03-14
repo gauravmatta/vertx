e79a3335b2708fe2943ec121372f50e18e6c11e1
gauravmatta <gaurav.matta.tech@gmail.com>
2025-03-14 08:42:05 +0530

    updated Batch Response

--- vertx-big-board/src/main/java/com/vertximplant/starter/vertx_big_board/handler/PutWatchListDatabaseHandler.java
+++ vertx-big-board/src/main/java/com/vertximplant/starter/vertx_big_board/handler/PutWatchListDatabaseHandler.java
@@ -5,6 +5,8 @@ import com.google.inject.Inject;
 import com.vertximplant.starter.vertx_big_board.api.WatchListRestAPI;
 import com.vertximplant.starter.vertx_big_board.helper.DBResponseHelper;
 import com.vertximplant.starter.vertx_big_board.pojo.WatchList;
+import com.vertximplant.starter.vertx_big_board.pojo.exception.Failure;
+import io.netty.handler.codec.http.HttpResponseStatus;
 import io.vertx.core.json.JsonObject;
 import io.vertx.ext.web.RoutingContext;
 import io.vertx.sqlclient.Pool;
@@ -41,12 +43,10 @@ public class PutWatchListDatabaseHandler {
         .executeBatch(parameterBatch).onFailure(DBResponseHelper.errorHandler(routingContext,
             "Failed to insert watchlist for accountId: " + accountId))
         .onSuccess(result -> {
-          dbResponseHelper.handleSuccessResponse("t_test", routingContext, result, stopwatch);
-          // dbResponseHelper.handleEmptyResponse("t_test", routingContext, "WatchListId",
-          // new Failure(HttpResponseStatus.NO_CONTENT.code(), ""), "Insert in WatchList",
-          // stopwatch);
-          //
+           dbResponseHelper.handleEmptyResponse("t_test", routingContext, "WatchListId",
+           new Failure(HttpResponseStatus.NO_CONTENT.code(), ""), "Insert in WatchList",
+           stopwatch);
+
         });
-    // });
   }
 }
