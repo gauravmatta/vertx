d1977a558774e18bf4bb65f1793d5024c190075a
Gaurav Matta <gaurav.matta.tech@gmail.com>
2025-03-19 10:31:30 +0530

    Added Uni

---
+++ vertx-mutiny/src/main/java/com/vertximplant/mutiny/vertx_mutiny/HelloMulti.java
@@ -0,0 +1,12 @@
+package com.vertximplant.mutiny.vertx_mutiny;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class HelloMulti {
+  private static final Logger LOG = LoggerFactory.getLogger(HelloMulti.class);
+
+  public static void main(String[] args) {
+
+  }
+}
