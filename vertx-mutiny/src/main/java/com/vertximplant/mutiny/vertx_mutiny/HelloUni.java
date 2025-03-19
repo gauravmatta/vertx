050cea71114ded8f07a475d64b34776e418d8c56
Gaurav Matta <gaurav.matta.tech@gmail.com>
2025-03-19 10:32:54 +0530

    Formatted files

--- vertx-mutiny/src/main/java/com/vertximplant/mutiny/vertx_mutiny/HelloUni.java
+++ vertx-mutiny/src/main/java/com/vertximplant/mutiny/vertx_mutiny/HelloUni.java
@@ -9,19 +9,11 @@ public class HelloUni {
   private static final Logger LOG = LoggerFactory.getLogger(HelloUni.class);
 
   public static void main(String[] args) {
-    //Uni represents a stream that can only emit either an item or a failure
-    Uni.createFrom().item("Hello")
-      .onItem().transform(item -> item+" Mutiny!")
-      .onItem().transform(String::toUpperCase)
-      .subscribe().with(
-        item->LOG.info("Item: {}",item)
-      );
+    // Uni represents a stream that can only emit either an item or a failure
+    Uni.createFrom().item("Hello").onItem().transform(item -> item + " Mutiny!").onItem()
+        .transform(String::toUpperCase).subscribe().with(item -> LOG.info("Item: {}", item));
 
-    Uni.createFrom().item("Ignored due to Failure")
-      .onItem().castTo(Integer.class)
-      .subscribe().with(
-        item -> LOG.info("Item: {}",item),
-      failure -> LOG.error("Failed with: ",failure)
-      );
+    Uni.createFrom().item("Ignored due to Failure").onItem().castTo(Integer.class).subscribe()
+        .with(item -> LOG.info("Item: {}", item), failure -> LOG.error("Failed with: ", failure));
   }
 }
