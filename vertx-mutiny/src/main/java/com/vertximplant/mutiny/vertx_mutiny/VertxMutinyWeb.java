9f907f3239ccb87d1be40e732d4820d026d8f54e
Gaurav Matta <gaurav.matta.tech@gmail.com>
2025-03-21 10:06:15 +0530

    optimized code

--- vertx-mutiny/src/main/java/com/vertximplant/mutiny/vertx_mutiny/VertxMutinyWeb.java
+++ vertx-mutiny/src/main/java/com/vertximplant/mutiny/vertx_mutiny/VertxMutinyWeb.java
@@ -5,7 +5,6 @@ import io.smallrye.mutiny.vertx.core.AbstractVerticle;
 import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
 import io.vertx.mutiny.core.Vertx;
-import io.vertx.mutiny.core.http.HttpServer;
 import io.vertx.mutiny.ext.web.Router;
 import io.vertx.mutiny.ext.web.RoutingContext;
 import org.slf4j.Logger;
@@ -15,7 +14,8 @@ public class VertxMutinyWeb extends AbstractVerticle {
   private static final Logger LOG = LoggerFactory.getLogger(VertxMutinyWeb.class);
 
   public static void main(String[] args) {
-    Vertx.vertx().deployVerticle(new VertxMutinyWeb()).subscribe().with(id -> LOG.info("Started: {}", id));
+    Vertx.vertx().deployVerticle(new VertxMutinyWeb()).subscribe()
+        .with(id -> LOG.info("Started: {}", id));
   }
 
   @Override
@@ -24,15 +24,12 @@ public class VertxMutinyWeb extends AbstractVerticle {
     router.route().failureHandler(this::failureHandler);
     router.get("/users").respond(this::getUsers);
     router.get().respond(this::rootHandler);
-    return vertx.createHttpServer()
-      .requestHandler(router)
-      .listen(8111)
-      .replaceWithVoid();
+    return vertx.createHttpServer().requestHandler(router).listen(8111).replaceWithVoid();
   }
 
   private Uni<String> rootHandler(RoutingContext routingContext) {
     return Uni.createFrom().item("Hello").onItem().transform(item -> item + " Mutiny!").onItem()
-      .transform(String::toUpperCase);
+        .transform(String::toUpperCase);
   }
 
   private void failureHandler(RoutingContext routingContext) {
@@ -41,8 +38,8 @@ public class VertxMutinyWeb extends AbstractVerticle {
 
   private Uni<JsonArray> getUsers(RoutingContext routingContext) {
     final JsonArray responseBody = new JsonArray();
-    responseBody.add(new JsonObject().put("name","Gaurav"));
-    responseBody.add(new JsonObject().put("name","Raghavi"));
+    responseBody.add(new JsonObject().put("name", "Gaurav"));
+    responseBody.add(new JsonObject().put("name", "Raghavi"));
     return Uni.createFrom().item(responseBody);
   }
 }
--- vertx-mutiny/src/main/java/com/vertximplant/mutiny/vertx_mutiny/db/VertxMutinyReactiveSQL.java
+++ vertx-mutiny/src/main/java/com/vertximplant/mutiny/vertx_mutiny/db/VertxMutinyReactiveSQL.java
@@ -8,7 +8,6 @@ import io.vertx.core.json.JsonObject;
 import io.vertx.mutiny.core.Vertx;
 import io.vertx.mutiny.ext.web.Router;
 import io.vertx.mutiny.ext.web.RoutingContext;
-import io.vertx.mutiny.pgclient.PgPool;
 import io.vertx.mutiny.sqlclient.Pool;
 import io.vertx.mutiny.sqlclient.Row;
 import io.vertx.pgclient.PgConnectOptions;
@@ -16,7 +15,6 @@ import io.vertx.pgclient.impl.PgPoolOptions;
 import io.vertx.sqlclient.PoolOptions;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 import java.time.LocalTime;
 
 public class VertxMutinyReactiveSQL extends AbstractVerticle {
@@ -24,58 +22,49 @@ public class VertxMutinyReactiveSQL extends AbstractVerticle {
 
 
   public static void main(String[] args) {
-    DeploymentOptions options = new DeploymentOptions().setConfig(new JsonObject().put("port",5432));
-    Vertx.vertx().deployVerticle(VertxMutinyReactiveSQL::new,options).subscribe().with(id -> LOG.info("Started: {}", id));
+    DeploymentOptions options =
+        new DeploymentOptions().setConfig(new JsonObject().put("port", 5432));
+    Vertx.vertx().deployVerticle(VertxMutinyReactiveSQL::new, options).subscribe()
+        .with(id -> LOG.info("Started: {}", id));
   }
 
   @Override
   public Uni<Void> asyncStart() {
     Router router = Router.router(vertx);
-    vertx.setPeriodic(5000L,item -> LOG.info("{}", LocalTime.now().getMinute()));
+    vertx.setPeriodic(5000L, item -> LOG.info("{}", LocalTime.now().getMinute()));
     Pool db = createPgPool(config());
     router.route().failureHandler(this::failureHandler);
     router.get("/assets").respond(routingContext -> executeQuery(db));
     router.get().respond(this::rootHandler);
-    return vertx.createHttpServer()
-      .requestHandler(router)
-      .listen(8111)
-      .replaceWithVoid();
+    return vertx.createHttpServer().requestHandler(router).listen(8111).replaceWithVoid();
   }
 
   private Uni<JsonArray> executeQuery(Pool db) {
     LOG.info("Executing DB query to find all users...");
-    return db
-      .query("SELECT * FROM broker.assets")
-      .execute()
-      .onItem().transform(rows ->{
-        JsonArray data = new JsonArray();
-        for(Row row : rows) {
-          data.add(row.toJson());
-        }
-        LOG.info("Return data: {}",data);
-        return data;
-        })
-      .onFailure()
-      .invoke(failure -> LOG.error("Failed query: ",failure))
-      .onFailure()
-      .recoverWithItem(new JsonArray());
+    return db.query("SELECT * FROM broker.assets").execute().onItem().transform(rows -> {
+      JsonArray data = new JsonArray();
+      for (Row row : rows) {
+        data.add(row.toJson());
+      }
+      LOG.info("Return data: {}", data);
+      return data;
+    }).onFailure().invoke(failure -> LOG.error("Failed query: ", failure)).onFailure()
+        .recoverWithItem(new JsonArray());
   }
 
   private Pool createPgPool(JsonObject config) {
     PgConnectOptions connectOptions = new PgConnectOptions().setHost("localhost")
-      .setPort(config.getInteger("port"))
-      .setDatabase(EmbeddedPostgres.DATABASE_NAME)
-      .setUser(EmbeddedPostgres.USERNAME)
-      .setPassword(EmbeddedPostgres.PASSWORD);
+        .setPort(config.getInteger("port")).setDatabase(EmbeddedPostgres.DATABASE_NAME)
+        .setUser(EmbeddedPostgres.USERNAME).setPassword(EmbeddedPostgres.PASSWORD);
 
     PoolOptions poolOptions = new PgPoolOptions().setMaxSize(5);
 
-    return Pool.pool(vertx,connectOptions,poolOptions);
+    return Pool.pool(vertx, connectOptions, poolOptions);
   }
 
   private Uni<String> rootHandler(RoutingContext routingContext) {
     return Uni.createFrom().item("Hello").onItem().transform(item -> item + " Mutiny!").onItem()
-      .transform(String::toUpperCase);
+        .transform(String::toUpperCase);
   }
 
   private void failureHandler(RoutingContext routingContext) {
