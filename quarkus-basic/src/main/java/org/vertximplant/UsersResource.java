b0beed1978fcb84bc07b85b5282aedafa6864c80
Gaurav Matta <gaurav.matta.tech@gmail.com>
2025-04-30 12:03:49 +0530

    Added create endpoint

--- quarkus-basic/src/main/java/org/vertximplant/UsersResource.java
+++ quarkus-basic/src/main/java/org/vertximplant/UsersResource.java
@@ -1,17 +1,17 @@
 package org.vertximplant;
 
+import io.quarkus.hibernate.reactive.panache.Panache;
 import io.quarkus.panache.common.Sort;
 import io.smallrye.mutiny.Uni;
 import jakarta.enterprise.context.ApplicationScoped;
-import jakarta.ws.rs.Consumes;
-import jakarta.ws.rs.GET;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.Response;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.vertximplant.entity.Users;
 
 
+import java.net.URI;
 import java.util.List;
 
 @Path("/users")
@@ -26,5 +26,21 @@ public class UsersResource {
     public Uni<List<Users>> get() {
         LOG.info("Get all users...");
         return Users.listAll(Sort.by("id"));
+    }
+
+    @GET
+    @Path("/{id}")
+    public Uni<Users> getById(Long id){
+        LOG.info("Get By id: {}",id);
+        return Users.findById(id);
+    }
+
+    @POST
+    public Uni<Response> create(Users user){
+        LOG.info("Create: {}",user);
+        return Panache.<Users>withTransaction(user::persist)
+                .onItem().transform(insertedUser ->
+                        Response.created(URI.create("/users"+insertedUser.id)).build()
+                );
     }
 }
